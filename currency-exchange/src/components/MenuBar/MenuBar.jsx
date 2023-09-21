032b1c2f84813582d6ebd4bba7e03f2fbad642fd
ignatiusaa <ignatius.a.ambrose@gmail.com>
2023-09-22 07:31:07 +0800

    update menu button

--- currency-exchange/src/components/MenuBar/MenuBar.jsx
+++ currency-exchange/src/components/MenuBar/MenuBar.jsx
@@ -4,30 +4,39 @@ import { Link } from "react-router-dom";
 export default function MenuBar() {
     const [showMenu, setShowMenu] = useState(false);
 
-    const toggleMenu = () => {
-        setShowMenu(!showMenu);
+    const handleMouseEnter = () => {
+        setShowMenu(true)
     }
+
+    const handleMouseLeave = () => {
+        setShowMenu(false)
+    }
+    
 
     return (
         <div className="menu-position fixed top-0 right-0 p-10">
-            <div className="bg-sky-100 rounded-lg border-solid border-2 border-teal-200 menu-container text-3xl text-black underline p-4 font-bold">
-                <p onClick = {toggleMenu}>Menu</p>
-                {showMenu && (
-                    <ul className="absolute right-0 mt-2 border rounded-lg shadow-lg bg-teal-50">
-                    <li>
-                        <Link to="/" className="block px-4 py-2 hover:bg-sky-50 border-2 border-solid border-slate-300 " >Main Page</Link>
-                    </li>
-                    <li>
-                        <Link to="/mywallet" className="block px-4 py-2 hover:bg-sky-50 border-2 border-solid border-slate-300">My Wallet</Link>
-                    </li>
-                    <li>
-                        <Link to="/chartpage" className="block px-4 py-2 hover:bg-sky-50 border-2 border-solid border-slate-300">Historical Charts</Link>
-                    </li>
-                    <li>
-                        <Link to="/about" className="block px-4 py-2 hover:bg-sky-50 border-2 border-solid border-slate-300">About</Link>
-                    </li>
-                </ul>
-                )}
+            <div className="bg-sky-100 rounded-lg border-solid border-2 border-teal-200 menu-container text-3xl text-black underline p-4 font-bold"
+                onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
+                >
+                <div>
+                    <p>Menu</p>
+                    {showMenu && (
+                        <ul className="absolute right-0 mt-2 border rounded-lg shadow-lg bg-teal-50">
+                        <li>
+                            <Link to="/" className="block px-4 py-2 hover:bg-sky-50 border-2 border-solid border-slate-300 " >Main Page</Link>
+                        </li>
+                        <li>
+                            <Link to="/mywallet" className="block px-4 py-2 hover:bg-sky-50 border-2 border-solid border-slate-300">My Wallet</Link>
+                        </li>
+                        <li>
+                            <Link to="/chartpage" className="block px-4 py-2 hover:bg-sky-50 border-2 border-solid border-slate-300">Historical Charts</Link>
+                        </li>
+                        <li>
+                            <Link to="/about" className="block px-4 py-2 hover:bg-sky-50 border-2 border-solid border-slate-300">About</Link>
+                        </li>
+                    </ul>
+                    )}
+                </div>
             </div>
         </div>   
     )
