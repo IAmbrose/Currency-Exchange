9aab00031d04c266b29746131cde714ebf4b985c
ignatiusaa <ignatius.a.ambrose@gmail.com>
2023-09-22 07:42:18 +0800

    minor update

--- currency-exchange/src/components/MenuBar/MenuBar.jsx
+++ currency-exchange/src/components/MenuBar/MenuBar.jsx
@@ -18,25 +18,23 @@ export default function MenuBar() {
             <div className="bg-sky-100 rounded-lg border-solid border-2 border-teal-200 menu-container text-3xl text-black underline p-4 font-bold"
                 onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
                 >
-                <div>
-                    <p>Menu</p>
-                    {showMenu && (
-                        <ul className="absolute right-0 mt-2 border rounded-lg shadow-lg bg-teal-50">
-                        <li>
-                            <Link to="/" className="block px-4 py-2 hover:bg-sky-50 border-2 border-solid border-slate-300 " >Main Page</Link>
-                        </li>
-                        <li>
-                            <Link to="/mywallet" className="block px-4 py-2 hover:bg-sky-50 border-2 border-solid border-slate-300">My Wallet</Link>
-                        </li>
-                        <li>
-                            <Link to="/chartpage" className="block px-4 py-2 hover:bg-sky-50 border-2 border-solid border-slate-300">Historical Charts</Link>
-                        </li>
-                        <li>
-                            <Link to="/about" className="block px-4 py-2 hover:bg-sky-50 border-2 border-solid border-slate-300">About</Link>
-                        </li>
-                    </ul>
-                    )}
-                </div>
+                <p>Menu</p>
+                {showMenu && (
+                    <ul className="absolute right-0 mt-2 border rounded-lg shadow-lg bg-teal-50">
+                    <li>
+                        <Link to="/" className="block px-4 py-2 hover:bg-sky-50 border-2 border-solid border-slate-300 " >Main Page</Link>
+                    </li>
+                    <li>
+                        <Link to="/mywallet" className="block px-4 py-2 hover:bg-sky-50 border-2 border-solid border-slate-300">My Wallet</Link>
+                    </li>
+                    <li>
+                        <Link to="/chartpage" className="block px-4 py-2 hover:bg-sky-50 border-2 border-solid border-slate-300">Historical Charts</Link>
+                    </li>
+                    <li>
+                        <Link to="/about" className="block px-4 py-2 hover:bg-sky-50 border-2 border-solid border-slate-300">About</Link>
+                    </li>
+                </ul>
+                )}
             </div>
         </div>   
     )
