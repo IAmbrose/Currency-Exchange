1dc5e2d757a4caf609341a6f92a63090c4a49fba
ignatiusaa <ignatius.a.ambrose@gmail.com>
2023-09-21 17:17:29 +0800

    minor css styling update

--- currency-exchange/src/components/AboutPage/AboutPage.jsx
+++ currency-exchange/src/components/AboutPage/AboutPage.jsx
@@ -1,8 +1,8 @@
 export default function AboutPage () {
     return(
         <>
-        <div className="bg-teal-100 ">
-            <h1 className="bg-teal-500 text-5xl font-serif text-black mt-12 p-4 font-bold">About the App</h1>
+        <div className="bg-teal-100 rounded-lg">
+            <h1 className="bg-teal-500 text-5xl font-serif text-black mt-12 p-4 font-bold rounded-lg">About the App</h1>
             <div className="font-serif text-black p-4 text-justify max-w-sm">
                 An application for users who frequently travels overseas and needs to manage different currencies. 
                 This application is a currency converter which quickly and easily convert between 
