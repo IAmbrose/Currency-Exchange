558b467a9c7e7722eea3a02d9a36c6e7e392258c
ignatiusaa <ignatius.a.ambrose@gmail.com>
2023-09-21 13:19:35 +0800

    minor styling issue

--- currency-exchange/src/components/ConverterMainPage/CurrencyContainer.jsx
+++ currency-exchange/src/components/ConverterMainPage/CurrencyContainer.jsx
@@ -1,7 +1,7 @@
 export default function CurrencyContainer ({ currencies, selectCurrency, onChangeCurrency }) {
     
   return (
-    <select className="cursor-pointer w-25 h-6 p-1" value={selectCurrency} onChange={onChangeCurrency}>
+    <select className="cursor-pointer w-25 h-6" value={selectCurrency} onChange={onChangeCurrency}>
       {currencies.map(currency => (
         <option key={currency} value={currency}>
           {currency}
